568d72aae836af642bfdd1df577f4f500065e608
= <=>
2025-01-12 17:10:10 -0700

    Add examples of how to revalidatePath

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,18 +1,31 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { addMessage } from '@/lib/messages';
-import { revalidatePath } from 'next/cache';
+import { addMessage } from "@/lib/messages";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
-    'use server';
+    "use server";
 
-    const message = formData.get('message');
+    const message = formData.get("message");
     addMessage(message);
 
-    // we can revalidate cache here
-    revalidatePath();
-    redirect('/messages');
+    // we can revalidate cache here (revalidate a piece of cache on-demand)
+    // a bit more nuanced than using config (eg. using something like revalidate: 5 -
+    // which is a bit more brute - and may not be necessary to do)
+
+    // this will revalidate the cache for this path, but not any nested pages
+    //revalidatePath("/messages");
+
+    // revalidate cache for this path *and* any nested pages
+    //revalidatePath("/messages", "layout");
+
+    // revalidate cache for all pages on this site
+    //revalidatePath("/", "layout");
+
+    revalidateTag("msg");
+
+    redirect("/messages");
   }
 
   return (
