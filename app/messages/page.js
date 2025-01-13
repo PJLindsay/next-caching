c286f03325eb69a86668d45b158ecdbf5986b8c9
= <=>
2025-01-12 17:04:03 -0700

    Use revalidatePath instead

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,16 +1,17 @@
-import { unstable_noStore } from "next/cache";
+//import { unstable_noStore } from "next/cache";
 
 import Messages from "@/components/messages";
 
 // file-level next.js config to say revalidate cache after 5 seconds
 //export const revalidate = 5;
+
 //next's force-dynamic is same as cache: no-store == always refresh
+// if you use this - you won't see the o beside this component when you npm run build
 //export const dynamic = "force-dynamic";
 
 export default async function MessagesPage() {
   // same effect as force-dynamic (just inside this specific component and any requests sent to data sources)
   //unstable_noStore();
-
   const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
